51f48ebfc437f6d9053db6fca3a6cd7fe167f579
George Paradox <paradox81ru@yandex.ru>
2025-08-12 14:25:51 +0300

    Update test_game.py

--- three_doors_challenge/tests/test_game.py
+++ three_doors_challenge/tests/test_game.py
@@ -3,7 +3,7 @@ import unittest
 from pathlib import Path
 
 sys.path.append(str(Path.cwd()))
-from three_doors_challenge.game import Game
+from three_doors_challenge.game import Game, GameResult
 
 
 class TestChallenge(unittest.TestCase):
@@ -11,36 +11,26 @@ class TestChallenge(unittest.TestCase):
         """ Тестироует открытие ведушим двери с козой. """
         game = Game()
         for i in range(1, 21):
-            game.hidding_car()
-            game.gamer_select_door()
-            game.host_opens_door()
-            result = game.get_result()            
+            game._hidding_car()
+            game._gamer_select_door()
+            game._host_opens_door()
+            result = GameResult(game._car_door, game._selected_door, game._opened_to_presenter, game._opened_door)           
             with self.subTest(f"check {i}: {result}"):
                 self.assertNotIn(result.opened_to_presenter, set([result.car_door, result.selected_door]))
-            game.reset()
+            game._reset()
                 
     def test_gamer_opens_same_door(self):
         """ Тестирует открытие игроком той же двери, что и выбрал. """
         game = Game()
         for i in range(1, 21):
-            game.hidding_car()
-            game.gamer_select_door()
-            game.host_opens_door()
-            game.gamer_opens_same_door()
-            result = game.get_result()            
+            result = game.gamer_opens_same_door()         
             with self.subTest(f"check {i}: {result}"):
                 self.assertEqual(result.opened_door, result.selected_door)
-            game.reset()
     
     def test_gamer_opens_another_door(self):
         """ Тестирует открытие игроком другой двери. """
         game = Game()
         for i in range(1, 21):
-            game.hidding_car()
-            game.gamer_select_door()
-            game.host_opens_door()
-            game.gamer_opens_another_door()
-            result = game.get_result()            
+            result = game. gamer_opens_another_door()        
             with self.subTest(f"check {i}: {result}"):
                 self.assertNotIn(result.opened_door, set([result.opened_to_presenter, result.selected_door]))
-            game.reset()
