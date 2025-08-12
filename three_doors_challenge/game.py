175e1334bc66bbddfba48ed8f0b5c10507dfa1fd
George Paradox <paradox81ru@yandex.ru>
2025-08-12 11:18:26 +0300

    Update and rename challenge.py to game.py

---
+++ three_doors_challenge/game.py
@@ -0,0 +1,61 @@
+from dataclasses import dataclass
+import random
+
+@dataclass(frozen=True)
+class GameResult:
+    # Номер двери с автомобилем.
+    car_door: int
+    # Выбранная дверь.
+    selected_door: int
+    # Дверь открытая ведущим
+    opened_to_presenter: int
+    # В результате открытая дверь.
+    opened_door: int
+
+class Game:
+    def __init__(self):
+        # Номер двери с автомобилем.
+        self._car_door = 0
+        # Выбранная дверь.
+        self._selected_door = 0
+        # Дверь открытая ведущим.
+        self._opened_to_presenter = 0
+        # В результате открытая дверь.
+        self._opened_door = 0
+
+    def reset(self):
+        """ Сброс выбора. """
+        self._car_door = 0
+        self._selected_door = 0
+        self._opened_to_presenter = 0
+        self._opened_door = 0
+
+    def hidding_car(self):
+        """ Прячет автомобиль за дверью. """
+        self._car_door = random.randint(1, 3)
+
+    def gamer_select_door(self):
+        """ Игрок выбирает случайную дверь. """
+        self._selected_door = random.randint(1, 3)
+
+    def host_opens_door(self):
+        """ Ведущий открывает дверь  с козой. """
+        doors = set(range(1, 4))
+        # Из выбора удаляется дверь с автомобитлем.        
+        doors.discard(self._car_door)
+        # Из выбора удаляется дверь выбранная игроком.        
+        doors.discard(self._selected_door)
+
+        self._opened_to_presenter = random.choice(tuple(doors))
+
+    def gamer_opens_same_door(self):
+        """ Игрок окрывает туже дверь. """
+        self._opened_door = self._selected_door
+
+    def gamer_opens_another_door(self):
+        """ Игрок открывает другую дверь. """
+        self._opened_door = (set(range(1, 4)) - set([self._selected_door, self._opened_to_presenter])).pop()
+
+    def get_result(self) -> GameResult:
+        """ Возвращает результаты """
+        return GameResult(self._car_door, self._selected_door, self._opened_to_presenter, self._opened_door)
